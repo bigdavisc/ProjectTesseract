ec3e776cb45a838646739b6f24c4ef0858f09f9d
Joe Zlonicky <joe.zlonicky@gmail.com>
2019-10-11 20:45:33 -0700

    Improved model and functionality

--- Portal.gd
+++ Portal.gd
@@ -1,10 +1,19 @@
 extends Area
 
-export var newPos = Vector3()
+export var connectedPortal = "PortalName"
+var used = false
 
 func _ready():
 	pass
 
 func _on_Portal_body_entered(body):
-	if body.is_in_group("Player"):
-		body.translation = newPos
+	if not used and body.is_in_group("Player"):
+		print("Entered portal")
+		for portal in get_tree().get_nodes_in_group("Portal"):
+			print("Checking portal: " + portal.get_name())
+			if portal.get_name() == connectedPortal:
+				body.translation = portal.translation
+				portal.used = true
+
+func _on_Portal_body_exited(body):
+	used = false
