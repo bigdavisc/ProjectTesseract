82733ede4d4a903957c8416c8516f820f336514e
Joe Zlonicky <joe.zlonicky@gmail.com>
2019-10-12 17:15:30 -0700

    Use portal rotation on player, added visual for editor to easily see portal rotation

--- Portal.gd
+++ Portal.gd
@@ -1,19 +1,20 @@
 extends Area
 
 export var connectedPortal = "PortalName"
+
 var used = false
 
 func _ready():
-	pass
+	$RotationVisualizer.visible = false
 
 func _on_Portal_body_entered(body):
 	if not used and body.is_in_group("Player"):
-		print("Entered portal")
 		for portal in get_tree().get_nodes_in_group("Portal"):
-			print("Checking portal: " + portal.get_name())
 			if portal.get_name() == connectedPortal:
-				body.translation = portal.translation
+				body.translation = portal.get_global_transform().origin
+				body.rotation.y = portal.get_global_transform().basis.get_euler().y
 				portal.used = true
+				return
 
 func _on_Portal_body_exited(body):
 	used = false
