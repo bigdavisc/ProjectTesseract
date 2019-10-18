e2c77ea0b413db1bcb45ad558e968eb1dee7dc32
SirDuck145 <matthiasharden@gmail.com>
2019-10-17 19:22:03 -0700

    Fix Port and Ip

--- Lobby.gd
+++ Lobby.gd
@@ -3,12 +3,13 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-const DEFAULT_IP = "127.0.0.1" #IP
+const DEFAULT_IP = "127.0.0.1"
 const DEFAULT_PORT = 25565
 const MAX_PLAYERS = 20
 
 var players = { }
 var self_data = { name = '', position = Vector3(360, 180, 0)}
+var goalIP = "10.0.0.231"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_player_connected")
@@ -24,7 +25,7 @@ func _player_connected(id):
 func _on_buttonHost_pressed():
 	print("Hosting network")
 	var host = NetworkedMultiplayerENet.new()
-	var res = host.create_server(4242, MAX_PLAYERS)
+	var res = host.create_server(DEFAULT_PORT, MAX_PLAYERS)
 	if res != OK:
 		print("Error creating server")
 		return
@@ -36,7 +37,7 @@ func _on_buttonHost_pressed():
 func _on_buttonJoin_pressed():
 	print("Joining network")
 	var host = NetworkedMultiplayerENet.new()
-	host.create_client("127.0.0.1",4242)
+	host.create_client(goalIP,DEFAULT_PORT)
 	get_tree().set_network_peer(host)
 	$buttonHost.hide()
 	$buttonJoin.disabled = true
